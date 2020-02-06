302e3bbdde5d07caefb238533e7fa508cce74046
Othman Bouzahzah <othmanbouzahzah@gmail.com>
2020-02-06 14:50:00 +0100

    Update detect_somme_argent.py

--- NLP/detect_somme_argent.py
+++ NLP/detect_somme_argent.py
@@ -1,6 +1,6 @@
-# Détection des sommes d'argent
-def detect_somme_argent(phrase):
+# Money detection
+def detect_amount_money(phrase):
     
     pattern = r"(\s|^)([\d]+(,[\s]*[\d]+)?)[\s]*(keuros|euros|ke|keuro|keu|e)($|\s)"
     
-    return re.sub(pattern, ' SOMMEARGENT ', phrase)
+    return re.sub(pattern, ' amount_money ', phrase)
